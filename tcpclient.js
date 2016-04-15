b0b81ecc43396b324d7c9c62e03753b5a0006c9e
admin <hoangtd.ptd@gmail.com>
2016-04-15 17:33:07 +0700

    chay duoc pressure

--- tcpclient.js
+++ tcpclient.js
@@ -10,8 +10,9 @@ var client = net.connect({server:server,port:serverPort},function(){
 	console.log('client connected');
 	console.log('send data to server');
 	var id = ids[Math.floor(Math.random()*ids.length)];
-	client.write(id + ',BP' + ',' + Math.random() * 100 + ',' + Math.random() * 100 + ',' + Math.random() * 100);
-	//client.end();
+	//client.write(id + ',BP' + ',' + Math.random() * 100 + ',' + Math.random() * 100 + ',' + Math.random() * 100);
+	client.write(id + ',TEMP' + ',' + Math.random() * 100);
+	client.end();
 });
 
 client.on('data', function(data) {
